72856bff54af9cf1538d7cf7d3dbf9afd3dc9b38
Atom <chanidapa.atom@gmail.com>
2025-12-09 15:58:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x=1, even=0,odd=0;
+    while (x!=0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if (x==0){
+            break;
+        }else if (x%2==0){
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = "<< even <<endl;
+    cout << "#Odd numbers = "<< odd <<endl;
     return 0;
 }
